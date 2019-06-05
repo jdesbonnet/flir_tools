95d99ff2cdbe8b13e6f443ce7f4c83bbaf8af30d
Joe Desbonnet <jdesbonnet@gmail.com>
2019-06-05 01:25:28 +0100

    no comment

--- extract_images.sh
+++ extract_images.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+FLIR_TOOL=flir_tool
 FLIR_JPEG="$1"
-base_name="$(basename -- $FLIR_JPEG .jpg)"
+
+s="$FLIR_JPEG"
+base_name="${FLIR_JPEG%.*}"
+
 
 exiftool "$FLIR_JPEG" -b -EmbeddedImage > "${base_name}_visible.jpg"
-exiftool "$FLIR_JPEG" -b -RawThermalImage | convert - -interlace none -depth 16 r:- | ./flir_tool > "${base_name}_radiometric.pgm"
+exiftool "$FLIR_JPEG" -b -RawThermalImage | convert - -interlace none -depth 16 r:- | ${FLIR_TOOL} > "${base_name}_radiometric.pgm"
 
