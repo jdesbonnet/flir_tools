e1795b4fa6c2d68ab6f8a80ee0098c2c194d23fa
Joe Desbonnet <jdesbonnet@gmail.com>
2020-09-29 21:45:41 +0100

    update for Flir A320

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flir_tools
-FLIR E4/E8 camera data extractions tools
+
+Tools to convert raw radiometric image from Flir Ex (E4, E8 etc) and Flir A320 cameras into estimated tempartures using open sources 
+components.  These tool makes use of exiftool to extract radiometic data and constants from the image file.  Flir JPEG and SEQ files 
+are supported.
+
--- flir_tool.c
+++ flir_tool.c
@@ -2,21 +2,27 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <math.h>
 
 /**
  * Convert raw radiometric data from FLIR E4/E8 thermal camera
  * into 16 bit grey where value = temperature_C*10;
  * Get raw radiomatric data like so:
- * exiftool $TIR_IMG -RawThermalImage -b | convert - -interlace none -depth 16 r:-  > radiometric.raw
  *
  * Reference: http://u88.n24.queensu.ca/exiftool/forum/index.php/topic,4898.msg23944.html#msg23944
  *
  * Dependencies: exiftools, ImageMagick
  *
- * Joe Desbonnet 4 May 2019.
+ * Joe Desbonnet 2020-09-24.
  */
 
+
+#define UNIT_C 1
+#define UNIT_K 2
+
+#define FORMAT_PGM 2
+#define FORMAT_PGM_ASCII 5
 
 /**
  * Help on how to use this utility.
@@ -26,6 +32,11 @@ static void usage (char *cmd)
 	printf ("%s [options] <spi-device>\n", cmd);
 	printf ("Options:\n");
 	printf ("  -d <debug_level>: debug level: 0=no debug (default), 9=most verbose\n");
+	printf ("  -C <key>=<value>: set constant, key can be one of R1,R2,B,O,F. Example -C R2=123\n");
+	printf ("  -f <format>: output format, one of pgm or pgm_ascii. Default pgm. \n");
+	printf ("  -u <unit>: output unit, one of C (Celsius) K (Kelvin). Default K.\n");
+	printf ("  -m <n>: multiplier. Output is value (16 bit unsigned int) is value * multiplier.\n");
+	printf ("          Example: with -u C -m 100 31.4C would be represented by value 3140\n");
 }
 
 static void version () 
@@ -38,13 +49,29 @@ int main (int argc, char **argv) {
 	uint32_t adc;
 
 
+	// output unit
+	int unit = UNIT_C;
+
+	int format = FORMAT_PGM;
+
+
 	// These constants come from the image EXIF metadata
 	// eg exiftool -Pl* flir_image.jpg
-	const double R1 = 14226.111;
-	const double R2 = 0.027153991;
-	const double O  = -7518;
-	const double B  = 1387.2;
-	const double F  = 2.5;
+	// Todo: allow to specify from command line
+
+	// From Flir E4
+	// double R1 = 14226.111;
+	// double R2 = 0.027153991;
+	// double O  = -7518;
+	// double B  = 1387.2;
+	// double F  = 2.5;
+
+	// From Flir A320
+        double R1 = 14809.436;
+	double R2 = 0.010635848;
+	double B = 1379.6;
+	double O = -5846;
+	double F = 1;
 
 
 	double T,S,C;
@@ -56,13 +83,49 @@ int main (int argc, char **argv) {
 
 	// Parse command line arguments. See usage() for details.
 	int c;
-	while ((c = getopt(argc, argv, "b:d:f:g:hio:qs:tv")) != -1) {
+	while ((c = getopt(argc, argv, "C:b:d:f:g:hio:qs:tv")) != -1) {
 		switch(c) {
 
-
+			case 'C': {
+				// constants in form var=val, eg R2=123.45. Locate the '=' character
+				// and replace with string terminator (0). String to right is the
+				// value which needs to be parsed as double. The string from optarg
+				// to where the equal was is the key.
+				char *s;
+				s = strchr(optarg,'=');
+				*(s++) = 0; // replace '=' with terminator, val is to the right of where '=' was
+				double val = atof (s);
+				char* key = optarg;
+				//fprintf (stderr,"%s = %f\n", key, val);
+				if (strcmp(key,"R1")==0) {
+					R1 = val;
+				} else if (strcmp(key,"R2")==0) {
+					R2 = val;
+				} else if (strcmp(key,"B")==0) {
+					B = val;
+				} else if (strcmp(key,"O")==0) {
+					O = val;
+				} else if (strcmp(key,"F")==0) {
+					F = val;
+				} else {
+					fprintf (stderr, "unrecognized constant %s\n",key);
+				}
+				break;
+				}
 			case 'd':
 				debug_level = atoi (optarg);
 				break;
+
+			case 'f':
+				if (strcmp(optarg,"pgm")==0) {
+					format = FORMAT_PGM;
+				} else if (strcmp(optarg,"pgm_ascii")==0) {
+					format = FORMAT_PGM_ASCII;
+				} else {
+					fprintf (stderr, "unrecognized format %s\n",optarg);
+				}
+				break;
+
 
 			case 'h':
 				version();
@@ -72,6 +135,15 @@ int main (int argc, char **argv) {
 
 			case 'm':
 				multiplier = atof (optarg);
+				break;
+
+
+			case 'u':
+				if (strcmp(optarg,"C")==0) {
+					unit = UNIT_C;
+				} else if (strcmp(optarg,"K")==0) {
+					unit = UNIT_K;
+				}
 				break;
 
 			case 'v':
@@ -86,34 +158,78 @@ int main (int argc, char **argv) {
 		}
 	}
 
+	// Read PGM header of raw radiometric file
+	int pgm_width, pgm_height, pgm_maxval;
+
+	while(getc(stdin) != '\n'); // skip file magic number ('P5')
+   
+	// ignore comment lines
+	while (getc(stdin) == '#') {
+		while (getc(stdin) != '\n');          
+	}
+	fseek(stdin, -1, SEEK_CUR);
+	fscanf(stdin,"%d", &pgm_width);
+	fscanf(stdin,"%d", &pgm_height);
+	fscanf(stdin,"%d", &pgm_maxval);
+
+
+	// Output header
+	if (format == FORMAT_PGM) {
+		fprintf (stdout,"P2 %d %d\n%d", pgm_width, pgm_height, pgm_maxval);
+	} else if (format == FORMAT_PGM_ASCII) {
+		fprintf (stdout,"P5 %d %d\n%d", pgm_width, pgm_height, pgm_maxval);
+	}
 
 
 
-
-	fprintf (stdout, "P5 320 240 65535\n");
+	int i = 0,vi;
+	double vo;
 
 	while (!feof(stdin)) {
 
 		fread (&v, sizeof v, 1, stdin);
-
-		adc = ((v&0xff)<<8)  | (v>>8);
+		//adc = ((v&0xff)<<8)  | (v>>8);
+		adc = v;
 
 		S = (double)adc;
 
-
 		// Temperature in K
+		// Q: what about F?
 		T = B / log(R1/(R2*(S+O))+1);
 
-		// Temperature in Celsius
-		C = T - 273.15;
+		switch (unit) {
+			case UNIT_K:
+			vo = T;
+			break;
+			case UNIT_C:
+			vo = T = 273.15;
+			break;
+		}
+		vo *= multiplier;
 
-		//fprintf(stderr,"%f ", T);
-		//fputc ((v&0xff), stdout);
-		//fputc (v>>8, stdout);
+		if (vo > 65535) {
+			vo = 65535;
+		} else if (vo < 0) {
+			vo = 0;
+		}
 
-		v = (uint16_t)(C*100);
-		fputc (v>>8,stdout);
-		fputc (v&0xff,stdout);
+		// NetPBM format uses bigendian ordering
+		// Ref https://en.wikipedia.org/wiki/Netpbm
+		//fputc (v>>8,stdout);
+		//fputc (v&0xff,stdout);
+
+		if (format == FORMAT_PGM_ASCII) {
+			if (i%320==0) {
+				fprintf(stdout,"\n");
+			}
+			fprintf (stdout, "%d ", (int)(vo));
+		} else {
+			// big endian order
+			vi = (int)vo;
+			fputc ((vi>>8) & 0xff, stdout);
+			fputc (vi & 0xff, stdout);
+		}
+		i++;
 
 	}
 }
